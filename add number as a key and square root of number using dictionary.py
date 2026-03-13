28b1fc45b410773f168e7c112fbb09541c4bfd88
nipunsinh25 <nipunsinh.jadeja125548@marwadiuniversity.ac.in>
2026-03-13 17:30:36 +0530

    Update add number as a key and square root of number using dictionary.py

--- add number as a key and square root of number using dictionary.py
+++ add number as a key and square root of number using dictionary.py
@@ -1,4 +1,7 @@
 # create dictionary in such a way that it should add number as a key and square root of number as a value
+# Enrollment Number : 92400527061
+# Name : Nipunsinh Jadeja
+
 
 n = int(input("Enter number: "))
 
@@ -8,3 +11,4 @@ for i in range(1,n+1):
     dictionary[i] = i ** 2
 
     print(dictionary)
+
