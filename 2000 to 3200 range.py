df17ef1ff3ab6a8583b55c780d9037928b92559c
nipunsinh25 <nipunsinh.jadeja125548@marwadiuniversity.ac.in>
2026-03-13 17:29:33 +0530

    Reformat code with comments and maintain functionality

--- 2000 to 3200 range.py
+++ 2000 to 3200 range.py
@@ -1,3 +1,8 @@
+# 7 but are not a multiple of 5, between 2000 and 3200
+# Enrollment Number : 92400527061
+# Name : Nipunsinh Jadeja
+
 for i in range(2000,3201):
     if i % 7 == 0 and i % 5 != 0 :
         print(i)
+
